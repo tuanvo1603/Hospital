9b93aeb70c58d802672282d7acda4809fa178484
tuanvo1603 <117852084+tuanvo1603@users.noreply.github.com>
2024-02-02 12:12:12 +0700

    Feature (#35)
    * modify prescription service
    
    * modify prescription service

--- src/main/java/project/hospital/api/treatment/prescriptiondetail/GetDistributedMedicationTodayApi.java
+++ src/main/java/project/hospital/api/treatment/prescriptiondetail/GetDistributedMedicationTodayApi.java
@@ -5,7 +5,6 @@ import project.hospital.api.Api;
 import project.hospital.constant.StatusCode;
 import project.hospital.dto.PrescriptionDetailDTO;
 import project.hospital.exception.EmployeeNotFoundException;
-import project.hospital.mapper.PrescriptionDetailMapper;
 import project.hospital.request.treatment.prescription.GetDistributedMedicationTodayRequest;
 import project.hospital.response.treatment.prescription.GetDistributedMedicationTodayResponse;
 import project.hospital.service.SessionService;
@@ -21,23 +20,19 @@ public class GetDistributedMedicationTodayApi extends Api<GetDistributedMedicati
 
     private final PrescriptionDetailService prescriptionDetailService;
 
-    private final PrescriptionDetailMapper prescriptionDetailMapper;
-
     public GetDistributedMedicationTodayApi(NurseService nurseService,
                                             PrescriptionDetailService prescriptionDetailService,
-                                            PrescriptionDetailMapper prescriptionDetailMapper,
                                             SessionService sessionService) {
         super(sessionService);
         this.nurseService = nurseService;
         this.prescriptionDetailService = prescriptionDetailService;
-        this.prescriptionDetailMapper = prescriptionDetailMapper;
     }
 
     @Override
     public GetDistributedMedicationTodayResponse execute(GetDistributedMedicationTodayRequest requestData) {
         try{
             nurseService.checkExistenceOfEmployee(requestData.getEmployeeId());
-            List<PrescriptionDetailDTO> prescriptionDetailDTOList = prescriptionDetailMapper.mapToPrescriptionDetailDTOList(prescriptionDetailService.getAllPrescriptionTodayByPatientId(requestData.getPatientId()));
+            List<PrescriptionDetailDTO> prescriptionDetailDTOList = prescriptionDetailService.getAllPrescriptionTodayByPatientId(requestData.getPatientId());
 
             return new GetDistributedMedicationTodayResponse(prescriptionDetailDTOList);
         }catch (EmployeeNotFoundException e) {
--- src/main/java/project/hospital/service/treatment/prescription/PrescriptionDetailService.java
+++ src/main/java/project/hospital/service/treatment/prescription/PrescriptionDetailService.java
@@ -3,12 +3,13 @@ package project.hospital.service.treatment.prescription;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import project.hospital.dto.PrescriptionDetailDTO;
 import project.hospital.exception.MedicationQuantityNotEnoughException;
+import project.hospital.mapper.PrescriptionDetailMapper;
 import project.hospital.model.treatment.medication.Medication;
 import project.hospital.model.treatment.medication.PrescriptionDetail;
 import project.hospital.repository.treatment.PrescriptionDetailRepository;
 import project.hospital.service.DateService;
-import project.hospital.service.treatment.HospitalFeeService;
 import project.hospital.service.treatment.IMoneyCountableService;
 
 import java.sql.Date;
@@ -23,14 +24,19 @@ public class PrescriptionDetailService implements IMoneyCountableService<Prescri
 
     private final MedicationService medicationService;
 
+    private final PrescriptionDetailMapper prescriptionDetailMapper;
+
     public PrescriptionDetailService(PrescriptionDetailRepository prescriptionDetailRepository,
-                                     DateService dateService, MedicationService medicationService) {
+                                     DateService dateService,
+                                     MedicationService medicationService,
+                                     PrescriptionDetailMapper prescriptionDetailMapper) {
         this.prescriptionDetailRepository = prescriptionDetailRepository;
         this.dateService = dateService;
         this.medicationService = medicationService;
+        this.prescriptionDetailMapper = prescriptionDetailMapper;
     }
 
-    private int countUsedMedicationQuantity(PrescriptionDetail  prescriptionDetail) {
+    private int countUsedMedicationQuantity(PrescriptionDetail prescriptionDetail) {
         int daysOfUsing = dateService.countDaysInPeriod(prescriptionDetail.getStartTime(), prescriptionDetail.getEndTime());
         return daysOfUsing * prescriptionDetail.getQuantityPerDay();
     }
@@ -39,25 +45,25 @@ public class PrescriptionDetailService implements IMoneyCountableService<Prescri
     public void createPrescriptionDetail(Long patientId, PrescriptionDetail prescriptionDetail) {
         Medication medication = medicationService.getMedicationById(prescriptionDetail.getMedicationId());
         int usedMedicationQuantity = this.countUsedMedicationQuantity(prescriptionDetail);
-        if(medicationService.isEnoughMedicationRemainingQuantity(medication, usedMedicationQuantity)) {
+        if (medicationService.isEnoughMedicationRemainingQuantity(medication, usedMedicationQuantity)) {
             medicationService.updateRemainingQuantity(medication, usedMedicationQuantity);
             prescriptionDetail.setPatientId(patientId);
             prescriptionDetailRepository.save(prescriptionDetail);
-        }
-        else throw new MedicationQuantityNotEnoughException();
+        } else throw new MedicationQuantityNotEnoughException();
     }
 
     public void deletePrescription(Long prescriptionDetailId) {
         prescriptionDetailRepository.deleteById(prescriptionDetailId);
     }
 
-    public List<PrescriptionDetail> getAllPrescriptionTodayByPatientId(Long patientId) {
+    public List<PrescriptionDetailDTO> getAllPrescriptionTodayByPatientId(Long patientId) {
         Date today = dateService.getToday();
         List<PrescriptionDetail> prescriptionDetailList = prescriptionDetailRepository.findAllByPatientId(patientId);
-        return prescriptionDetailList
-                .stream()
-                .filter(prescriptionDetail -> dateService.isDateBetweenPeriod(today, prescriptionDetail.getStartTime(), prescriptionDetail.getEndTime()))
-                .toList();
+        return prescriptionDetailMapper.mapToPrescriptionDetailDTOList(
+                prescriptionDetailList
+                        .stream()
+                        .filter(prescriptionDetail -> dateService.isDateBetweenPeriod(today, prescriptionDetail.getStartTime(), prescriptionDetail.getEndTime()))
+                        .toList());
     }
 
     @Override
